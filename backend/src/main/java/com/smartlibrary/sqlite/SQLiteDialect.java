e138b8549504195781edcbf3f2f0dad9f6b20ef9
Amin <amin.albooyeh@gmail.com>
2025-07-18 11:53:57 +0200

    updating for fixing some conflict

--- backend/src/main/java/com/smartlibrary/sqlite/SQLiteDialect.java
+++ backend/src/main/java/com/smartlibrary/sqlite/SQLiteDialect.java
@@ -2,7 +2,6 @@ package com.smartlibrary.sqlite;
 
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.identity.IdentityColumnSupport;
-import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
 
 public class SQLiteDialect extends Dialect {
     public SQLiteDialect() {
@@ -11,7 +10,26 @@ public class SQLiteDialect extends Dialect {
 
     @Override
     public IdentityColumnSupport getIdentityColumnSupport() {
-        return new IdentityColumnSupportImpl();
+        return new SQLiteIdentityColumnSupport();
+    }
+
+    // Custom IdentityColumnSupport for SQLite
+    public static class SQLiteIdentityColumnSupport extends org.hibernate.dialect.identity.IdentityColumnSupportImpl {
+        @Override
+        public boolean supportsIdentityColumns() {
+            return true;
+        }
+
+        @Override
+        public String getIdentitySelectString(String table, String column, int type) {
+            return "select last_insert_rowid()";
+        }
+
+        @Override
+        public String getIdentityColumnString(int type) {
+            // return the SQLite autoincrement syntax
+            return "integer primary key autoincrement";
+        }
     }
 
     @Override
